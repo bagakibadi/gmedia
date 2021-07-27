377774bc26687a7ae07ee5cd314a642170f5e962
Satomo <satriyo@ymail.com>
2021-07-27 15:36:56 +0700

    version update

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -270,7 +270,7 @@
 				</div>
 				<div class="foot-bot">
 					<p>©UPNVeteranYogyakarta</p>
-					<p style="opacity: .3">v0.5.5</p>
+					<p style="opacity: .3">v0.6.1</p>
 				</div>
 			</div>
 		</div>
